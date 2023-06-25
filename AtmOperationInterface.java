e511e8cbbd07de78e694c5af771b1b265e6cbcf2
Raadhikari <132909124+Raadhikari@users.noreply.github.com>
2023-06-25 22:24:33 +0530

    Add files via upload

---
+++ AtmOperationInterface.java
@@ -0,0 +1,8 @@
+package AtmProject;
+
+public interface AtmOperationInterface {
+	public void viewbalance();
+	public void withdraAmount(double withdrawAmount);
+	public void depositAmount(double depositeAmount);
+	public void viewMiniStatement();
+}
